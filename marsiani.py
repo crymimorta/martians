21705c931b05a2b93cfd0db2575e3860a800c536
Пользователь <bekture05@gmail.com>
2023-11-30 03:16:01 +0600

    checking total weight

--- marsiani.py
+++ marsiani.py
@@ -10,3 +10,7 @@ def check_cargo_location(locations, kilometers):
         if locations[i] != kilometers[i]:
             return False
     return True
+
+def check_total_weight(total_weight, weights):  # Function, that calculates the total weight of the cargo.
+    sum_weights = sum(weights[i] for i in range(3))
+    return total_weight == sum_weights
