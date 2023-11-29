da8ce1754947493371277617c911c3b6211ad54f
Пользователь <bekture05@gmail.com>
2023-11-30 03:11:29 +0600

    checking location

--- marsiani.py
+++ marsiani.py
@@ -3,3 +3,10 @@ import random
 def generate_cargo_location():  # Dist from spaceship to city 7 km
     return random.sample(range(1, 8), 3)
 
+def check_cargo_location(locations, kilometers):
+    if len(locations) != len(kilometers):
+        return False
+    for i in range(3):
+        if locations[i] != kilometers[i]:
+            return False
+    return True
